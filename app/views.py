76d8fd0a9643abe1969f37c8d2996f9eeec3336e
Talal Abou Haiba <talal.abouhaiba@gmail.com>
2020-04-30 22:01:39 -0700

    Fix redirects to view

--- app/views.py
+++ app/views.py
@@ -29,14 +29,14 @@ def register_with_param(type):
     if type == 'category':
         if request.method == 'POST' and form_category.category.data and form_category.validate():
             insertCategory(form_category.category.data)
-            return view()
+            return redirect('/view')
 
         return render_template('register:' + type + '.html', USER=USERNAME, form=form_category)
 
     elif type == 'item':
         if request.method == 'POST' and form_item.category.data and form_item.item.data and form_item.validate():
             insertItem(form_item.item.data) # TODO: Insert the category uuid and all other metadata
-            return view()
+            return redirect('/view')
 
         return render_template('register:' + type + '.html', USER=USERNAME, form=form_item, categories=getCategories())
 
