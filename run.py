cf0ce88e76166afa97326243d30b8e86bf8969f2
Talal Abou Haiba <talal.abouhaiba@gmail.com>
2020-09-26 21:43:44 -0700

    Added debug arg to activate ssl_context

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 # run.py
 
 from app import app
+import sys
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9205, ssl_context="adhoc")
+    if len(sys.argv) == 2 and sys.argv[1] is "d":
+        app.run(host='0.0.0.0', port=9205, ssl_context="adhoc")
+    else:
+        app.run(host='0.0.0.0', port=9205)
