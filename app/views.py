1f6dd461e161a673f06fc20caa36761048d0565d
Talal Abou Haiba <talal.abouhaiba@gmail.com>
2020-09-26 21:33:33 -0700

    Added login/logout to navbar. Using current_user to power personalized navbar and dashboard html

--- app/views.py
+++ app/views.py
@@ -22,8 +22,6 @@ from flask_login import (
 
 from .user import User
 
-USERNAME="Talal"
-
 # WIP AUTH
 GOOGLE_CLIENT_ID = os.environ.get("MAI_GOOGLE_CLIENT_ID", None)
 GOOGLE_CLIENT_SECRET = os.environ.get("MAI_GOOGLE_CLIENT_SECRET", None)
@@ -121,13 +119,11 @@ def logout():
 @app.route('/dashboard')
 def dashboard():
     print(str(current_user.is_authenticated))
-    print(current_user.name)
-    print(current_user.role)
-    return render_template('dashboard.html', USER=USERNAME)
+    return render_template('dashboard.html', USER=current_user)
 
 @app.route('/feedback')
 def feedback():
-    return render_template('feedback.html', USER=USERNAME)
+    return render_template('feedback.html', USER=current_user)
 
 @app.route('/register/<string:type>', methods=['GET', 'POST'])
 def register_with_param(type):
@@ -140,21 +136,21 @@ def register_with_param(type):
             insertCategory(form_category.category.data)
             return redirect('/dashboard')
 
-        return render_template('register:' + type + '.html', USER=USERNAME, form=form_category)
+        return render_template('register:' + type + '.html', USER=current_user, form=form_category)
 
     elif type == 'item':
         if request.method == 'POST' and form_item.category.data and form_item.item.data and form_item.validate():
             insertItem(str(form_item.category.data).strip(), str(form_item.item.data).strip(), str(form_item.location.data).strip())
             return redirect('/dashboard')
 
-        return render_template('register:' + type + '.html', USER=USERNAME, form=form_item, categories=getCategories())
+        return render_template('register:' + type + '.html', USER=current_user, form=form_item, categories=getCategories())
 
     elif type == 'barcode':
         if request.method == 'POST' and form_barcode.validate():
             insertBarcode(str(form_barcode.barcode.data).strip(), str(form_barcode.item.data).strip())
             return redirect('/dashboard')
 
-        return render_template('register:' + type + '.html', USER=USERNAME, form=form_barcode, categories=getCategories())
+        return render_template('register:' + type + '.html', USER=current_user, form=form_barcode, categories=getCategories())
 
     else:
         return redirect('/dashboard')
@@ -170,7 +166,7 @@ def register_barcode_for_item(uuid):
         insertBarcode(str(form_barcode.barcode.data).strip(), str(form_barcode.item.data).strip())
         return redirect('/view/item/' + uuid)
 
-    return render_template('register:' + 'barcode' + '.html', USER=USERNAME, form=form_barcode)
+    return render_template('register:' + 'barcode' + '.html', USER=current_user, form=form_barcode)
 
 @app.route('/edit/category/<string:uuid>', methods=['GET', 'POST'])
 def edit_category(uuid):
@@ -182,11 +178,11 @@ def edit_category(uuid):
     else:
         form_category.category.data = getCategoryForId(uuid)['name']
 
-    return render_template('edit:category.html', USER=USERNAME, form=form_category)
+    return render_template('edit:category.html', USER=current_user, form=form_category)
 
 @app.route('/delete/barcode')
 def delete_barcode_view():
-    return render_template('delete:barcode.html', USER=USERNAME, barcodes=getBarcodes())
+    return render_template('delete:barcode.html', USER=current_user, barcodes=getBarcodes())
 
 @app.route('/delete/barcode/<string:uuid>')
 def delete_barcode(uuid):
@@ -200,7 +196,7 @@ def delete_item(uuid):
 
 @app.route('/delete/category')
 def delete_category_view():
-    return render_template('delete:category.html', USER=USERNAME, categories=getDeletableCategories())
+    return render_template('delete:category.html', USER=current_user, categories=getDeletableCategories())
 
 @app.route('/delete/category/<string:uuid>')
 def delete_category(uuid):
@@ -223,23 +219,23 @@ def edit_item(uuid):
         form_item.notes.data = current_item['notes']
         form_item.url.data = current_item['url']
 
-    return render_template('edit:item.html', USER=USERNAME, form=form_item, item_name=current_item['name'])
+    return render_template('edit:item.html', USER=current_user, form=form_item, item_name=current_item['name'])
 
 @app.route('/view/category/<string:uuid>')
 def view_category(uuid):
-    return render_template('view:items.html', USER=USERNAME, category=getCategoryForId(uuid)['name'], items=getItemsForCategory(uuid))
+    return render_template('view:items.html', USER=current_user, category=getCategoryForId(uuid)['name'], items=getItemsForCategory(uuid))
 
 @app.route('/view/all')
 def view_all_items():
-    return render_template('view:items.html', USER=USERNAME, category='All items', items=getItems())
+    return render_template('view:items.html', USER=current_user, category='All items', items=getItems())
 
 @app.route('/view/item/<string:uuid>')
 def view_item(uuid):
-    return render_template('view:item.html', USER=USERNAME, item=getItemForId(uuid), barcodes=getBarcodesForItem(uuid))
+    return render_template('view:item.html', USER=current_user, item=getItemForId(uuid), barcodes=getBarcodesForItem(uuid))
 
 @app.route('/view')
 def view():
-    return render_template('view.html', USER=USERNAME, categories=getCategories())
+    return render_template('view.html', USER=current_user, categories=getCategories())
 
 @app.route('/barcode/check_in', methods=['GET', 'POST'])
 def barcode_check_in_item():
@@ -253,7 +249,7 @@ def barcode_check_in_item():
         else:
             flash("An error occurred when handling your request. Please validate the barcode and quantity.")
 
-    return render_template('scan:barcode.html', USER=USERNAME, form=form_barcode, action="Check in", defaultQuantity=1)
+    return render_template('scan:barcode.html', USER=current_user, form=form_barcode, action="Check in", defaultQuantity=1)
 
 @app.route('/barcode/check_out', methods=['GET', 'POST'])
 def barcode_check_out_item():
@@ -265,7 +261,7 @@ def barcode_check_out_item():
         else:
             flash("An error occurred when handling your request. Please validate the barcode and quantity.")
 
-    return render_template('scan:barcode.html', USER=USERNAME, form=form_barcode, action="Check out", defaultQuantity=-1)
+    return render_template('scan:barcode.html', USER=current_user, form=form_barcode, action="Check out", defaultQuantity=-1)
 
 @app.route('/barcode/lookup', methods=['GET', 'POST'])
 def barcode_look_up_item():
@@ -274,4 +270,4 @@ def barcode_look_up_item():
         item_id = getBarcode(form_barcode.barcode.data)['item_id']
         return redirect('/view/item/' + item_id)
 
-    return render_template('scan:barcode.html', USER=USERNAME, form=form_barcode, action="Look up")
+    return render_template('scan:barcode.html', USER=current_user, form=form_barcode, action="Look up")
