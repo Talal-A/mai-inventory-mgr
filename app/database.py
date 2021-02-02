fca2d6d2bfb85db96c8517b861026b16a7938fbd
Talal Abou Haiba <talal.abouhaiba@gmail.com>
2021-02-01 23:21:26 -0800

    [mai1] Creating sqlite tables on startup

---
+++ app/database.py
@@ -0,0 +1,48 @@
+import sqlite3
+import json 
+from datetime import datetime
+
+DB_PATH = './data/mai.db'
+
+# To be called ONCE from application startup
+def __init_db():
+    db_connection = sqlite3.connect(DB_PATH)
+    __check_table(db_connection)
+
+def __get_db():
+    db_connection = sqlite3.connect(DB_PATH)
+    db_connection.execute('pragma journal_mode=wal')
+    return db_connection
+
+def __check_table(db_connection):
+    cursor = db_connection.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS barcode (
+            barcode text, item_id text, 
+            UNIQUE(barcode)
+        )
+        """
+    )
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS category (
+            category_id text, name text,
+            UNIQUE(category_id)
+        )
+        """
+    )
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS history (
+            date int, type text, user text, event text
+        )
+        """
+    )
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS item (
+            item_id text, category_id text, name text, location text, quantity_active int, quantity_expired int, notes text, url text,
+            UNIQUE(item_id)
+        )
+        """
+    )
+    db_connection.commit()
+    cursor.close()
--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 # run.py
 
-from app import app
+from app import app, database
 import sys
 
 if __name__ == '__main__':
+    database.__init_db()
     if len(sys.argv) == 2 and sys.argv[1] is "d":
         app.run(host='0.0.0.0', port=9205, ssl_context="adhoc")
     else:
