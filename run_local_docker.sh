e48fb29b30cdcdd86723746022020e2377ba7984
Talal Abou Haiba <talal.abouhaiba@gmail.com>
2021-05-29 22:24:17 -0700

    [mai66] Using litestream to sync db

--- run_local_docker.sh
+++ run_local_docker.sh
@@ -5,5 +5,6 @@ docker build -t mai-inventory-mgr -f docker/Dockerfile .
 docker rm "mai-inventory-mgr"
 docker run --name="mai-inventory-mgr" \
      -e TZ="America/Los_Angeles" \
+     -e STAGE="beta" \
      -p 9205:9205 \
      -v ~/dev/mai-ucla-2/data:/data mai-inventory-mgr
