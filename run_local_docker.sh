9f4985725adf752871b6141a4fc3327341675b8c
Talal Abou Haiba <talal.abouhaiba@gmail.com>
2021-05-29 23:21:22 -0700

    Updating run_local_docker.sh to pull env variables from a file

--- run_local_docker.sh
+++ run_local_docker.sh
@@ -4,7 +4,6 @@ docker build -t mai-inventory-mgr -f docker/Dockerfile .
 
 docker rm "mai-inventory-mgr"
 docker run --name="mai-inventory-mgr" \
-     -e TZ="America/Los_Angeles" \
-     -e STAGE="beta" \
+     --env-file .secrets \
      -p 9205:9205 \
      -v ~/dev/mai-ucla-2/data:/data mai-inventory-mgr
