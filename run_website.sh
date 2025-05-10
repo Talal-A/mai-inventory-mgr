5d5c15bacb5d986724772c5d83024afcb54e3411
Talal Abou Haiba <talal.abouhaiba@gmail.com>
2025-05-10 10:19:53 -0700

    #3 - create data dir if it does not exist during run_website.sh

--- run_website.sh
+++ run_website.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+if [ -d "./data" ]; then
+  echo "Data directory exists."
+else
+  echo "Data directory does not exist."
+  mkdir data
+fi
+
+
 echo -e "\033[1;37m Running on https://127.0.0.1:9205/ \033[0m"
 export $(cat .secrets | xargs) && python3 run-website.py d
